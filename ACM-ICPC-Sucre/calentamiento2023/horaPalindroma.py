f5d33f59edf7f9423949bc86d5d5f059426fe436
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-06-06 10:37:18 -0400

    fixed hora palindroma

--- ACM-ICPC-Sucre/calentamiento2023/horaPalindroma.py
+++ ACM-ICPC-Sucre/calentamiento2023/horaPalindroma.py
@@ -1,18 +1,10 @@
-
-def hora_palindroma(hora: list) -> str:
-    hora_aux = []
-    for i in hora:
-        hora_aux.append(i[::-1])
-    return "SI" if hora == hora_aux[::-1] else "NO"
+def hora_palindroma(hora: str) -> str:
+    return "SI" if hora == hora[::-1] else "NO"
 
 
 if __name__ == "__main__":
-    data = []
     while True:
-        hora = input().split(" ")
-        if hora == ["0", "0"]:
+        hora = input().split()
+        if hora[0] == "0" and hora[1] == "0":
             break
-        data.append(hora)
-        
-    for hora in data:
-        print(hora_palindroma(hora))
+        print(hora_palindroma("".join(hora)))
