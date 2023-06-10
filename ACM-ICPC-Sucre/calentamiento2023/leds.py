662f60ef264faa0e35c2dd64cff9d8e6c8cef8ae
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-06-09 23:45:42 -0400

    fixed leds

--- ACM-ICPC-Sucre/calentamiento2023/leds.py
+++ ACM-ICPC-Sucre/calentamiento2023/leds.py
@@ -16,7 +16,7 @@ def calcular_leds(hora: str) -> str:
     cantidad_leds = 0
     for i in hora:
         cantidad_leds += DIGITOS[i]
-        
+
     if cantidad_leds % 2 == 0:
         return cantidad_leds, hora, '1' * (cantidad_leds // 2)
     maximo = '7'+'1' * (cantidad_leds // 2-1)
@@ -27,10 +27,10 @@ n = int(input())
 horas = []
 for _ in range(n):
     hh, mm = input().split()
+    if len(mm) == 1:
+        mm = '0' + mm
     horas.append(hh+mm)
 
 for i in horas:
     leds, hora, max_leds = calcular_leds(i)
-    print(leds)
-    print(hora)
-    print(max_leds)
+    print(f'{leds}\n{hora}\n{max_leds}')
