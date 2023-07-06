11add2dbc37593e6a5627f675d32a88dfce3697b
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-07-05 21:22:53 -0400

    fixed tytancod/escaleras

--- titancod/escaleras/escaleras.py
+++ titancod/escaleras/escaleras.py
@@ -1,25 +1,21 @@
-def maxima_altura(pilas):
-    suma = sum(pilas)
-    for i in range(1, len(pilas)+1):
-        suma_aux = sum(range(1, i+1))
-        if suma == suma_aux:
-            return i
+# url: https://www.titancod.net/problema/ESCALERAS
+
+def acomodar_cajas(altura: int, caso: list) -> int:
+    cajas = 0
+    for i in range(1, altura+1):
+        if caso[i-1] > i:
+            diferencia = caso[i-1]-i
+            cajas += diferencia
+    print(cajas)
 
 
-c = int(input())  
-contarador_array = []
+if __name__ == "__main__":
+    c = int(input())
+    array_casos = []
 
-for _ in range(c):
-    pilas = list(map(int, input().split()))
+    for _ in range(c):
+        casos = list(map(int, input().split()))
+        array_casos.append(casos)
 
-    altura_maxima = maxima_altura(pilas)
-    contador = 0  
-
-    for pila in pilas:
-        diferencia = altura_maxima - pila  
-        contador += diferencia  
-
-    contarador_array.append(contador)
-
-for contador in contarador_array:
-    print(contador)
+    for i in array_casos:
+        acomodar_cajas(i[0], i[1:])
