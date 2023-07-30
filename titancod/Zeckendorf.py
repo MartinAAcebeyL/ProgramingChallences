a6283f4ec8ecd28b3752c57dfc91c48df7838b7d
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-07-30 11:26:23 -0400

    Zeckendorf-titancod

---
+++ titancod/Zeckendorf.py
@@ -0,0 +1,21 @@
+
+def fibonacci(n: int = 109) -> list:
+    fib_list = [0, 1]
+    while len(fib_list) < n:
+        next_number = fib_list[-1] + fib_list[-2]
+        fib_list.append(next_number)
+    return fib_list
+
+
+PRIMOS = fibonacci(40)
+
+number = int(input())
+
+
+while number != 0:
+    for i in PRIMOS[::-1]:
+        if i <= number:
+            number -= i
+            print(i, end=" ")
+            break
+print()
