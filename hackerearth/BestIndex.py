f2996eb3ded80472a8669f53a2277812735b92dc
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-09-06 16:32:02 -0400

    best index

---
+++ hackerearth/BestIndex.py
@@ -0,0 +1,18 @@
+N = int(input())
+ARRAY = [int(i) for i in input().split()]
+
+
+def sumatoria(indice: int) -> int:
+    iteraciones = (N-indice)//3
+    if iteraciones != 0:
+        return sum(ARRAY[indice:indice+(3*iteraciones)])
+    return sum(ARRAY[indice:indice+1])
+
+
+may = 0
+for i in range(N):
+    suma = sumatoria(i)
+    print(suma)
+    if suma > may:
+        may = suma
+print(may)
