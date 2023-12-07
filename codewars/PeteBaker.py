0340b224241fb8f7b76763efd87ae8538c528ccf
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-12-07 15:32:22 -0400

    codewars: peteBaker

---
+++ codewars/PeteBaker.py
@@ -0,0 +1,26 @@
+recipe = {"flour": 500, 'sugar': 200, 'eggs': 1}
+available = {"flour": 1200, 'sugar': 1200, 'eggs': 5, "milk": 200}
+
+# {'cream': 1, 'flour': 3, 'sugar': 1, 'milk': 1, 'oil': 1, 'eggs': 1}
+# {'cream': 1, 'flour': 3, 'sugar': 1, 'milk': 1, 'oil': 1, 'eggs': 1 }
+
+
+def cakes(recipe, available):
+    amounts = []
+    for ingredients in recipe:
+        recipe_amount = recipe.get(ingredients)
+        available_amount = available.get(ingredients, None)
+        if available_amount is None or available_amount < recipe_amount:
+            return 0
+        amount = 0
+        while available_amount >= recipe_amount:
+            available_amount -= recipe_amount
+            amount += 1
+        amounts.append(amount)
+
+    return min(amounts)
+
+
+_cakes = cakes(recipe, available)
+
+print(_cakes)
