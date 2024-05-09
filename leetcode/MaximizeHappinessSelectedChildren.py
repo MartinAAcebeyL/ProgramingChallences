8a672efa13498ad77a77a82957e5120fdff9cfbc
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2024-05-08 23:35:04 -0400

    leetcode: Maximize Happiness of Selected Children

---
+++ leetcode/MaximizeHappinessSelectedChildren.py
@@ -0,0 +1,20 @@
+# url: https://leetcode.com/problems/maximize-happiness-of-selected-children/description/?envType=daily-question&envId=2024-05-09
+from typing import List
+
+
+def maximumHappinessSum(happiness: List[int], k: int) -> int:
+    happiness.sort(reverse=True)
+    index = 0
+    counter = 0
+    for _ in range(k):
+        if happiness[index] - index >= 1:
+            counter += happiness[index] - index
+        else:
+            counter += happiness[index]
+        index += 1
+    return counter
+
+
+print(maximumHappinessSum([1, 2, 3], 2))  # 4
+print(maximumHappinessSum([1, 1, 1, 1], 2))  # 1
+print(maximumHappinessSum([2, 3, 4, 5], 1))  # 5
